6151e846011ca04dc4d2a67f896d2558d5637000
SagarSikchi <sikchisagar9330@gmail.com>
2021-03-13 20:38:47 +0530

    Update Main.java

--- Java Assignments/Problem Statement 2/Main.java
+++ Java Assignments/Problem Statement 2/Main.java
@@ -3,9 +3,10 @@
 */
 
 import java.util.*;
+import java.io.*;
 
 class Grocery {
-    private Map<String, ArrayList<String>> list = new HashMap<String, ArrayList<String>>();
+    private Map<String, ArrayList<String>> list = new LinkedHashMap<String, ArrayList<String>>();
 
     String lowercaseName(String name) {
         return name.toLowerCase();
@@ -18,7 +19,7 @@ class Grocery {
         System.out.printf("%-15s", "Price");
         System.out.println();
 
-        for(String key : list.keySet()) {
+        for (String key : list.keySet()) {
             System.out.printf("%-15s", key);
             System.out.printf("%-15s", list.get(key).get(0));
             System.out.printf("%-15s", list.get(key).get(1));
@@ -29,8 +30,8 @@ class Grocery {
 
     void leftBudgetBuy(int leftBudget) {
         String string = "\nAmount left can buy you ";
-        for(String key : list.keySet()) {
-            if(Integer.parseInt(list.get(key).get(1)) == leftBudget) {
+        for (String key : list.keySet()) {
+            if (Integer.parseInt(list.get(key).get(1)) == leftBudget) {
                 System.out.println(string + key);
                 break;
             }
@@ -39,7 +40,7 @@ class Grocery {
 
     int validate(int originalBudget) {
         int usedBudget = 0;
-        for(String key : list.keySet()) {
+        for (String key : list.keySet()) {
             usedBudget += Integer.parseInt(list.get(key).get(1));
         }
         return originalBudget - usedBudget;
@@ -47,7 +48,7 @@ class Grocery {
 
     void addProduct(String product, String quantity, String price) {
         product = lowercaseName(product);
-        if(list.containsKey(product)) {
+        if (list.containsKey(product)) {
             list.get(product).set(0, quantity);
             list.get(product).set(1, price);
         } else {
@@ -57,49 +58,60 @@ class Grocery {
     }
 
 }
+
 public class Main {
     public static void execute() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your budget: ");
-        int budget = scanner.nextInt();
-        int choice, originalBudget = budget;
-        String product, quantity, price;
-        Grocery grocery = new Grocery();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.print("Enter your budget: ");
+            Boolean isBreak = true;
+            int budget = Integer.parseInt(br.readLine());
+            int choice, originalBudget = budget;
+            String product = "", quantity = "", price = "";
+            Grocery grocery = new Grocery();
 
-        while(true) {
-            System.err.print("\n1. Add an item\n2. Exit\nEnter your choice: ");
-            choice = scanner.nextInt();
-            switch(choice) {
+            while (isBreak) {
+                System.out.print("\n1. Add an item\n2. Exit\nEnter your choice: ");
+                choice = Integer.parseInt(br.readLine());
+                switch (choice) {
                 case 1:
-                System.out.print("\nEnter product: ");
-                product = scanner.next();
-                scanner.nextLine();
-                System.out.print("Enter quantity: ");
-                quantity = scanner.next();
-                scanner.nextLine();
-                System.out.print("Enter price: ");
-                price = scanner.next();
-                scanner.nextLine();
-                if(Integer.parseInt(price) > budget) {
-                    System.out.println("\nCan't Buy the product ###(because budget left is " + budget + ")");
-                } else {
-                    grocery.addProduct(product, quantity, price);
-                    budget = grocery.validate(originalBudget);
-                    System.out.print("\nAmount left: " + budget + "\n");
-                }
-                break;
+                    System.out.print("\nEnter product: ");
+                    product = br.readLine();
+                    
+                    System.out.print("Enter quantity: ");
+                    quantity = br.readLine();
+                    
+                    System.out.print("Enter price: ");
+                    price = br.readLine();
+                    
+                    // System.out.println(product);
+                    // System.out.println(quantity);
+                    // System.out.println(price);
+
+                    if (Integer.parseInt(price) > budget) {
+                        System.out.println("\nCan't Buy the product ###(because budget left is " + budget + ")");
+                    } else {
+                        grocery.addProduct(product, quantity, price);
+                        budget = grocery.validate(originalBudget);
+                        System.out.print("\nAmount left: " + budget + "\n");
+                    }
+                    break;
                 case 2:
-                scanner.close();
-                // budget = grocery.validate(originalBudget);
-                grocery.leftBudgetBuy(budget);
-                grocery.printList();
-                return;
+                    isBreak = false;
+                    grocery.leftBudgetBuy(budget);
+                    grocery.printList();
+                    break;
                 default:
-                System.out.println("\nInvalid Choice!");
-                break;
+                    System.out.println("\nInvalid Choice!");
+                    break;
+                }
             }
+        } catch (Exception e) {
+            System.out.println("Error occured..");
+            return;
         }
     }
+
     public static void main(String[] args) {
         execute();
     }
